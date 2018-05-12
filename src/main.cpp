6c8d6c4cdc6ed574e14d991c316e854938ad17b0
Eric <eric_tj@126.com>
2018-05-12 08:18:39 +0800

    Add support on output folders

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <dirent.h>
 #include <errno.h>
+#include "disk_info.h"
 #include "FolderInfo.h"
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -17,6 +19,20 @@ typedef enum
     eFileType_Unknown,
     eFileType_UnSupported,
 }eFileType;
+
+int is_folder_exist(const std::string &path)
+{
+    struct stat sb;
+    if (stat(path.c_str(), &sb) == -1)
+        return 0;
+    if ((sb.st_mode & S_IFMT) == S_IFDIR)
+        return 1;
+    else
+    {
+        std::cerr << "Not folder: " << path << std::endl;
+        return -EINVAL;
+    }
+}
 
 eFileType get_file_type(const std::string &path)
 {
@@ -52,28 +68,51 @@ eFileType get_file_type(const std::string &path)
     }
 }
 
-void print_folder_info(FolderInfo &info)
+void print_cd_info(DiscInfo *disk)
+{
+    std::cout << "Disk Title: " << disk->title << std::endl;
+    std::cout << "Disk Performer: " << disk->performer << std::endl;
+    std::cout << "Num of tracks: " << disk->tracks.size() << std::endl;
+    std::cout << "Track Info:" << std::endl;
+    for (auto i = disk->tracks.begin(); i != disk->tracks.end(); i++)
+    {
+        auto track = *i;
+        std::cout << "--Track: " << track.index << std::endl;
+        std::cout << "\rtitle: " << track.title << std::endl;
+        std::cout << "\rperformer: " << track.performer << std::endl;
+        std::cout << "\rfile: " << track.file << std::endl;
+        for (auto j = track.time_index.begin();
+                j != track.time_index.end(); j++)
+        {
+            std::cout << "\rtime index: " << *j << std::endl;
+        }
+    }
+}
+
+bool is_media_folder(FolderInfo &info)
 {
     if (info.filename_cue.size() == 0 && info.filename_music.size() == 0)
     {
-        return;
+        return false;
     }
+    /*
     std::cout << "Folder: " << info.root_path << std::endl;
     std::cout << "\r - Num of cues: " << info.filename_cue.size() << std::endl;
     std::cout << "\r - Num of medias: " << info.filename_music.size() << std::endl;
+    */
+    return true;
 }
 
-int process_dir(const std::string &path)
+int clean_folder(const std::string &src_path)
 {
     DIR *dir;
     struct dirent *ptr;
-    FolderInfo folder_info(path);
+    int ret = 0;
 
-    //std::cout << "Processing Folder: " << path << std::endl;
-    dir = opendir(path.c_str());
+    dir = opendir(src_path.c_str());
     if (dir == nullptr)
     {
-        std::cout << "Dir open err: " << path << std::endl;
+        std::cout << "Dir open err: " << src_path << std::endl;
         return -EINVAL;
     }
     while ((ptr = readdir(dir)) != nullptr)
@@ -83,7 +122,122 @@ int process_dir(const std::string &path)
             continue;
 
         //std::cout << "--name: " << ptr->d_name << std::endl;
-        eFileType type = get_file_type(path + "/" + ptr->d_name);
+        filename = src_path + "/" + ptr->d_name;
+        eFileType type = get_file_type(filename);
+        if (type == eFileType_Folder)
+        {
+            ret = clean_folder(filename);
+        }
+        else
+        {
+            ret = remove(filename.c_str());
+        }
+        if (ret != 0)
+        {
+            std::cerr << "Failed remove: " << filename << std::endl;
+            break;
+        }
+    }
+out:
+    closedir(dir);
+    return ret;
+}
+
+int create_folder(const std::string &src_path)
+{
+    std::istringstream iss(src_path);
+    std::string s, path_;
+    int ret;
+
+    while(getline(iss, s, '/' ))
+    {
+        if (s.size() == 0)
+            continue;
+        path_ += "/" + s;
+        ret = is_folder_exist(path_);
+        if (ret == 1)
+            continue;
+        else if (ret < 0)
+        {
+            std::cerr << "Invalid folder: " << path_ << std::endl;
+            return ret;
+        }
+        else
+        {
+            ret = mkdir(path_.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+            if (ret != 0)
+            {
+                std::cerr << "Failed create folder: " << path_ << std::endl;
+                return ret;
+            }
+        }
+    }
+    return 0;
+}
+
+int make_clean_folder(std::string &path)
+{
+    struct stat sb;
+    int ret = 0;
+
+    if (stat(path.c_str(), &sb) == 0) //already exists
+    {
+        ret = clean_folder(path);
+        if (ret)
+            return ret;
+    }
+    ret = create_folder(path);
+    return ret;
+}
+
+int convert_cue_files(const std::string &dest_root,
+        DiscInfo &disk_info,
+        FolderInfo &folder_info,
+        const std::string &cue_file_name)
+{
+    std::string work_root = dest_root + "/" +
+        folder_info.root_path + "/" +
+        cue_file_name;
+    int ret;
+
+    std::cout << "Preparing Folder: " << work_root << std::endl;
+    ret = make_clean_folder(work_root);
+    if (ret != 0)
+    {
+        return ret;
+    }
+
+    for (int i = 0; i < disk_info.tracks.size(); i++)
+    {
+        TrackInfo *track_info = &disk_info.tracks[i];
+        //std::string media_file = folder_info.root_path + "/" + track_info->file;
+        std::string media_file = track_info->file;
+        std::cout << media_file << std::endl;
+        //TODO: Check file existen, and start convert
+    }
+}
+
+int process_dir(const std::string &src_path, const std::string &dst_path)
+{
+    DIR *dir;
+    struct dirent *ptr;
+    FolderInfo folder_info(src_path);
+
+    //std::cout << "Processing Folder: " << path << std::endl;
+    dir = opendir(src_path.c_str());
+    if (dir == nullptr)
+    {
+        std::cout << "Dir open err: " << src_path << std::endl;
+        return -EINVAL;
+    }
+    while ((ptr = readdir(dir)) != nullptr)
+    {
+        std::string filename(ptr->d_name);
+        if (filename == "." || filename == "..")
+            continue;
+
+        //std::cout << "--name: " << ptr->d_name << std::endl;
+        eFileType type = get_file_type(src_path + "/" + ptr->d_name);
         switch (type)
         {
             case eFileType_Media:
@@ -93,15 +247,40 @@ int process_dir(const std::string &path)
                 folder_info.add_cue(filename);
                 break;
             case eFileType_Folder:
-                process_dir(path + "/" + ptr->d_name);
+                process_dir(src_path + "/" + ptr->d_name, dst_path);
                 break;
             default:
                 break;
         }
     }
     closedir(dir);
-    //Test
-    print_folder_info(folder_info);
+
+    if (is_media_folder(folder_info) == false)
+    {
+        return 0;
+    }
+
+    /*
+     * First Process Cue File.
+     * The remain media files will be converted in whole
+     */
+    for (auto cue_index = folder_info.filename_cue.begin();
+            cue_index != folder_info.filename_cue.end();
+            cue_index++)
+    {
+        DiscInfo disc_info;
+        std::string full_path = folder_info.root_path + "/" + *cue_index;
+
+        std::cout << "Processing " << full_path << std::endl;
+
+        disc_info.parse_file(full_path);
+
+        convert_cue_files(dst_path, disc_info, folder_info, (*cue_index).substr(
+                    0, (*cue_index).find_last_of('.')));
+    }
+    /*
+     * Process remaining Media Files
+     */
     return 0;
 }
 
@@ -138,6 +317,6 @@ int main(int argc, char *argv[])
     std::cout << "Dst Folder: " << dst_folder << std::endl;
 
     //Test
-    process_dir(src_folder);
+    process_dir(src_folder, dst_folder);
     return 0;
 }
